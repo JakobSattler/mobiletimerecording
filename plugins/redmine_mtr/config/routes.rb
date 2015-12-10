2930dd5822969276281e04945ecc60babbe82276
Jakob Sattler <j.sattler97@gmail.com>
2015-12-10 16:06:21 +0100

    hallo

--- plugins/redmine_mtr/config/routes.rb
+++ plugins/redmine_mtr/config/routes.rb
@@ -2,5 +2,5 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 match 'mtr', :to => 'mtr_welcome#mtr_index', :as => 'mtr_home'
-match 'mtr/login', :to => 'mtr_account#mtr_login', :as => 'mtr_signin'
+match 'mtr/login', :to => 'mtr_account#mtr_login', :as => 'signin'
 match 'mtr/logout', :to => 'mtr_account#mtr_logout', :as => 'mtr_signout', :via => [:get, :post]
